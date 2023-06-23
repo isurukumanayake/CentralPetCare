f454e851c3cd34a3cd00059b450c7ae3b1ccc88b
IT21267772 <it21267772@my.sliit.lk>
2023-06-23 22:58:03 +0530

    Updated origin

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ const port = process.env.PORT || 4000;
 
 const app = express();
 
-app.use(cors());
+app.use(cors({origin: [process.env.CLIENT_URL, process.env.CLIENT_URL2]}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 

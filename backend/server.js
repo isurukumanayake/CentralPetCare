eea6caf736b17c5520b4cf5b928708fe9e8c3d95
IT21267772 <it21267772@my.sliit.lk>
2023-06-23 19:25:15 +0530

    Backend origin updated

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ const port = process.env.PORT || 4000;
 
 const app = express();
 
-app.use(cors());
+app.use(cors({origin: process.env.CLIENT_URL}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
